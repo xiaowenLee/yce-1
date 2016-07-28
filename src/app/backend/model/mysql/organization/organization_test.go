72cb94f2570f8363f8f7b908504ef710c77c3937
lth2015 <18910648861@163.com>
2016-07-28 18:17:08 +0800

    add update sql

--- src/app/backend/model/mysql/organization/organization_test.go
+++ src/app/backend/model/mysql/organization/organization_test.go
@@ -22,3 +22,18 @@ func Test_QueryOrganizationById(*testing.T) {
 	fmt.Printf("%v\n", org)
 
 }
+
+func Test_InsertOrganization(*testing.T) {
+
+	mysql.NewMysqlClient(mysql.DB_HOST, mysql.DB_USER, mysql.DB_PASSWORD, mysql.DB_NAME, mysql.MAX_POOL_SIZE)
+	mysql.MysqlInstance().Open()
+
+	o := NewOrganization("dev", "1000000.00", "1000000.00", "add dev org", 1000, 2000, 1)
+	o.InsertOrganization(2)
+
+	org := new(Organization)
+	org.QueryOrganizationById(2)
+
+	fmt.Printf("%v\n", org)
+
+}
