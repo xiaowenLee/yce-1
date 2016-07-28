5be6bf6ed2f2e2cf54f749206b0df6e13659bdc9
lth2015 <lth9379@gmail.com>
2016-07-29 01:50:59 +0800

    add modifiedOp to DcQuota

--- src/app/backend/model/mysql/dcquota/dcquota.go
+++ src/app/backend/model/mysql/dcquota/dcquota.go
@@ -4,21 +4,22 @@ import (
 	"big"
 	"cmd/compile/internal/big"
 )
-type DcQuota struct {
-	Id int32 `json:"id"`
-	Price  big.Rat `json:"price"`
-	DcId int32 `json:"dcId"`
-	OrgId int32 `json:"orgId"`
-	CreatedAt string `json:"createdAt`
-	ModifiedAt string `json:"modifiedAt"`
-	PodNumLimit int32 `json:"podNumLimit"`
-	PodCpuMax int32 `json:"podCpuMax"`
-	PodMemMax int32 `json:"podMemMax"`
-	PodCpuMin int32 `json:"podCpuMin"`
-	PodMemMin int32 `json:"podMemMin"`
-	RbdQuota int32 `json:"rbdQuota"`
-	podRbdMax int32 `json:"podRbdMax"`
-	podRbdMin int32 `json:"podRbdMin"`
-	Comment string `json:"comment"`
 
+type DcQuota struct {
+	Id          int32   `json:"id"`
+	Price       big.Rat `json:"price"`
+	DcId        int32   `json:"dcId"`
+	OrgId       int32   `json:"orgId"`
+	PodNumLimit int32   `json:"podNumLimit"`
+	PodCpuMax   int32   `json:"podCpuMax"`
+	PodMemMax   int32   `json:"podMemMax"`
+	PodCpuMin   int32   `json:"podCpuMin"`
+	PodMemMin   int32   `json:"podMemMin"`
+	RbdQuota    int32   `json:"rbdQuota"`
+	podRbdMax   int32   `json:"podRbdMax"`
+	podRbdMin   int32   `json:"podRbdMin"`
+	CreatedAt   string  `json:"createdAt`
+	ModifiedAt  string  `json:"modifiedAt"`
+	ModifedOp   int     `json:"modifiedOp"`
+	Comment     string  `json:"comment"`
 }
