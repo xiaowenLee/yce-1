30c2d3ccf76f3d16301ab5e0f90b883cb298fdf8
Jora <522205399@qq.com>
2016-07-30 01:10:52 +0800

    图标

--- src/app/frontend/scripts/mock/mockData.js
+++ src/app/frontend/scripts/mock/mockData.js
@@ -1,7 +1,7 @@
 Mock.mock('http://10.69.40.36.com/api/main/navlist', {
     list: [
-        {id: 1, name: 'Dashboard', state: 'main.dashboard',includeState: 'main.dashboard'},
-        {id: 2, name: '应用管理', state: 'main.appManage',includeState: 'main.appManage',
+        {id: 1, name: 'Dashboard', state: 'main.dashboard',includeState: 'main.dashboard',className:'fa-dashboard'},
+        {id: 2, name: '应用管理', state: 'main.appManage',includeState: 'main.appManage',className:'fa-adn',
             item: [
                 {id: 21, name: '发布', state: 'main.appManageDeployment', includeState: 'main.appManageDeployment'},
                 {id: 22, name: '回滚', state: 'main.appManageRollback', includeState: 'main.appManageRollback'},
@@ -10,20 +10,20 @@ Mock.mock('http://10.69.40.36.com/api/main/navlist', {
                 {id: 22, name: '历史发布', state: 'main.appManageHistory', includeState: 'main.appManageHistory'}
             ]
         },
-        {id: 3, name: '镜像管理', state: 'main.imageManage', includeState: 'main.imageManage',
+        {id: 3, name: '镜像管理', state: 'main.imageManage', includeState: 'main.imageManage',className:'fa-file-archive-o',
             item: [
                 {id: 31, name: '查找镜像', state: 'main.imageManageSearch', includeState: 'main.imageManageSearch'},
                 {id: 32, name: '删除镜像', state: 'main.imageManageDelete', includeState: 'main.imageManageDelete'}
             ]
         },
-        {id: 4, name: '云盘管理', state: 'main.cloudManage', includeState: 'main.cloudManage'},
-        {id: 5, name: '扩展功能', state: 'main.extensions', includeState: 'main.extensions',
+        {id: 4, name: '云盘管理', state: 'main.cloudManage', includeState: 'main.cloudManage',className:'fa-cloud'},
+        {id: 5, name: '扩展功能', state: 'main.extensions', includeState: 'main.extensions',className:'fa-arrows',
             item: [
                 {id: 51, name: '创建服务', state: 'main.extensionsService', includeState: 'main.extensionsService'},
                 {id: 52, name: '创建访问点', state: 'main.extensionsEndpoint', includeState: 'main.extensionsEndpoint'}
             ]
         },
-        {id: 6, name: '计费&充值', state: 'main.costManage', includeState: 'main.costManage'}
+        {id: 6, name: '计费&充值', state: 'main.costManage', includeState: 'main.costManage',className:'fa-credit-card'}
     ]
 });
 
