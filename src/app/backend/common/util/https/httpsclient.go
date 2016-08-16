871d1ca6b065644a1e083db744b5bf14c323d23d
lth2015 <lth9379@gmail.com>
2016-08-17 00:47:31 +0800

    add httpsClient

---
+++ src/app/backend/common/util/https/httpsclient.go
@@ -0,0 +1,35 @@
+package https
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	// "log"
+	"net/http"
+)
+
+type HttpsClient struct {
+	Host   string `json:"host"`
+	Port   int32  `json:"port"`
+	pool   x509.CertPool
+	client *http.Client
+}
+
+func NewHttpsClient(host string, port int32, cert string) *HttpsClient {
+
+	https := &HttpsClient{
+		Host: host,
+		Port: port,
+	}
+
+	https.pool.AppendCertsFromPEM([]byte(cert))
+
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{RootCAs: pool},
+		DisableCompression: true,
+	}
+
+	https.client = &http.Client{Transport: tr}
+
+	return https
+}
