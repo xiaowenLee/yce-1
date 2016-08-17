488259e96cd62d0c5268863d3dfedf32a3d02ba6
lth2015 <18910648861@163.com>
2016-08-17 18:05:44 +0800

    add domain.crt

--- src/app/backend/common/util/https/httpsclient.go
+++ src/app/backend/common/util/https/httpsclient.go
@@ -3,9 +3,9 @@ package https
 import (
 	"crypto/tls"
 	"crypto/x509"
-	// "log"
 	"net/http"
 	"fmt"
+	"io/ioutil"
 )
 
 type HttpsClient struct {
@@ -23,10 +23,13 @@ func NewHttpsClient(host, port, cert string) *HttpsClient {
 		pool: x509.NewCertPool(),
 	}
 
-	fmt.Printf("%v\n", https.pool)
-	// https.pool = new(x509.CertPool)
-	https.pool.AppendCertsFromPEM([]byte(cert))
+	caCrt, err := ioutil.ReadFile(cert)
+	if err != nil {
+		fmt.Println("ReadFile err:", err)
+		return nil
+	}
 
+	https.pool.AppendCertsFromPEM(caCrt)
 
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{RootCAs: https.pool},
