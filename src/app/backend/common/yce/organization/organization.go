f89a29e5618b15d2a52e5a9b1006eed88c14a4cf
maxwell92 <710028463@qq.com>
2016-08-17 21:14:29 +0800

    change OrgId to orgId

--- src/app/backend/common/yce/organization/organization.go
+++ src/app/backend/common/yce/organization/organization.go
@@ -13,26 +13,26 @@ type dcList struct {
 	DcList []string `json:"dcList"`
 }
 
-func DcHost(OrgId string) ([]string, error) {
+func DcHost(orgId string) ([]string, error) {
 	mysqlclient := mysql.MysqlInstance()
 	mysqlclient.Open()
 
 	myorganization := new(organization.Organization)
-	oid, err := strconv.Atoi(OrgId)
+	oid, err := strconv.Atoi(orgId)
 	if err != nil {
-		log.Printf("Get OrgId error: OrgId=%s, error=%s\n", OrgId, err)
+		log.Printf("Get orgId error: orgId=%s, error=%s\n", orgId, err)
 	}
 
 	err = myorganization.QueryOrganizationById(int32(oid))
 	if err != nil {
-		log.Printf("Get dcList error: OrgId=%s, error=%s\n", OrgId, err)
+		log.Printf("Get dcList error: orgId=%s, error=%s\n", orgId, err)
 		return nil, err
 	}
 
 	dclist := new(dcList)
 	err = json.Unmarshal([]byte(myorganization.DcList), &dclist)
 	if err != nil {
-		log.Printf("Decode dcList error: OrgId=%s, error=%s\n", OrgId, err)
+		log.Printf("Decode dcList error: orgId=%s, error=%s\n", orgId, err)
 		return nil, err
 	}
 
@@ -44,11 +44,11 @@ func DcHost(OrgId string) ([]string, error) {
 	for i := 0; i < num; i++ {
 		id, err := strconv.Atoi(dclist.DcList[i])
 		if err != nil {
-			log.Printf("Strconv.Atoi dcList error: OrgId=%s, error=%s\n", OrgId, err)
+			log.Printf("Strconv.Atoi dcList error: orgId=%s, error=%s\n", orgId, err)
 		}
 		err = mydatacenter.QueryDataCenterById(int32(id))
 		if err != nil {
-			log.Printf("Get dcHost error: OrgID=%s, error=%s\n", OrgId, err)
+			log.Printf("Get dcHost error: OrgID=%s, error=%s\n", orgId, err)
 		}
 		server[i] = mydatacenter.Host + ":" + strconv.Itoa(int(mydatacenter.Port))
 	}
