f359cb346e6553a18479bf846f66146be2677bb6
lth2015 <18910648861@163.com>
2016-08-19 17:29:41 +0800

    add router.go

--- src/app/backend/common/yce/router/router.go
+++ src/app/backend/common/yce/router/router.go
@@ -1,1 +1,29 @@
 package router
+
+import (
+	mydeploy "app/backend/controller/yce/deploy"
+	mylogin "app/backend/controller/yce/login"
+	mylogout "app/backend/controller/yce/logout"
+	mynavList "app/backend/controller/yce/navlist"
+	myregistry "app/backend/controller/yce/registry"
+	"github.com/kataras/iris"
+)
+
+type Router struct {
+}
+
+func (r *Router) Registe() {
+	login := new(mylogin.LoginController)
+	logout := new(mylogout.LogoutController)
+	nav := new(mynavList.NavListController)
+	listdeploy := new(mydeploy.ListDeployController)
+	registry := new(myregistry.ListRegistryController)
+
+	iris.API("/api/v1/users/login", *login)
+	iris.API("/api/v1/navlist", *nav)
+	iris.API("/api/v1/organizations/:orgId/users/:userId/deployments", *listdeploy)
+	iris.API("/api/v1/users/logout", *logout)
+	iris.API("/api/v1/registry/images", *registry)
+
+	iris.StaticServe("../frontend", "/static")
+}
--- src/app/backend/yce.go
+++ src/app/backend/yce.go
@@ -1,37 +1,24 @@
 package main
 
 import (
-	"app/backend/common/util/mysql"
-	mysession "app/backend/common/util/session"
-	mydeploy "app/backend/controller/yce/deploy"
-	mylogin "app/backend/controller/yce/login"
-	mylogout "app/backend/controller/yce/logout"
-	mynavList "app/backend/controller/yce/navlist"
-	myregistry "app/backend/controller/yce/registry"
-	"github.com/kataras/iris"
 	"log"
+	"github.com/kataras/iris"
+	"app/backend/common/util/mysql"
+	myrouter "app/backend/common/yce/router"
+	mysession "app/backend/common/util/session"
 )
 
-func main() {
-
+func init() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	mysql.NewMysqlClient(mysql.DB_HOST, mysql.DB_USER, mysql.DB_PASSWORD, mysql.DB_NAME, mysql.MAX_POOL_SIZE)
 	mysql.MysqlInstance().Open()
-
 	mysession.NewSessionStore()
+}
 
-	login := new(mylogin.LoginController)
-	logout := new(mylogout.LogoutController)
-	nav := new(mynavList.NavListController)
-	listdeploy := new(mydeploy.ListDeployController)
-	registry := new(myregistry.ListRegistryController)
+func main() {
 
-	iris.API("/api/v1/users/login", *login)
-	iris.API("/api/v1/navlist", *nav)
-	iris.API("/api/v1/organizations/:orgId/users/:userId/deployments", *listdeploy)
-	iris.API("/api/v1/users/logout", *logout)
-	iris.API("/api/v1/registry/images", *registry)
+	r := new(myrouter.Router)
+	r.Registe()
 
-	iris.StaticServe("../frontend", "/static")
 	iris.Listen(":8080")
 }
