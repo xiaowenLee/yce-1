569711355b62617e40868b6ec155ad8b3874b62b
maxwell92 <710028463@qq.com>
2016-09-12 17:33:36 +0800

    add time duration function

--- src/app/backend/common/util/time/time_test.go
+++ src/app/backend/common/util/time/time_test.go
@@ -9,3 +9,12 @@ func Test_LocalTime(*testing.T) {
 	l := NewLocalTime()
 	fmt.Printf("%s\n", l.String())
 }
+
+
+
+func Test_Sub(*testing.T) {
+	start := "2016-09-12T08:51:02Z"
+
+	fmt.Println(DurationFromUTC(start))
+
+}
