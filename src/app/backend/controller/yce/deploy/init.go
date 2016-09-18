2e5564988416c0fb9e328fe6a95c0d2d29706808
lth2015 <lth9379@gmail.com>
2016-09-18 19:46:57 +0800

    初步修改完init.go

--- src/app/backend/controller/yce/deploy/init.go
+++ src/app/backend/controller/yce/deploy/init.go
@@ -1,8 +1,6 @@
 package deploy
 
 import (
-	mylog "app/backend/common/util/log"
-	"app/backend/common/util/session"
 	myerror "app/backend/common/yce/error"
 	"app/backend/common/yce/organization"
 	myorganization "app/backend/model/mysql/organization"
@@ -10,52 +8,59 @@ import (
 	"app/backend/model/yce/deploy"
 	"encoding/json"
 	"github.com/kataras/iris"
+	yce "app/backend/controller/yce"
 )
 
 type InitDeployController struct {
-	*iris.Context
+	yce.Controller
 	org  *myorganization.Organization
 	Init deploy.InitDeployment
-	Ye *myerror.YceError
-}
-
-func (idc *InitDeployController) WriteBack() {
-	idc.Response.Header.Set("Access-Control-Allow-Origin", "*")
-	mylog.Log.Infof("CreateDeployController Response YceError: controller=%p, code=%d, note=%s", idc, idc.Ye.Code, myerror.Errors[idc.Ye.Code].LogMsg)
-	idc.Write(idc.Ye.String())
 }
 
 func (idc *InitDeployController) String() string {
 	data, err := json.Marshal(idc.Init)
 	if err != nil {
-		mylog.Log.Errorf("InitDeployController String() Marshal Error: err=%s", err)
+		log.Errorf("InitDeployController String() Marshal Error: err=%s", err)
 		return ""
 	}
 	return string(data)
 }
 
-// Validate Session
-func (idc *InitDeployController) validateSession(sessionId, orgId string) {
-	// Validate the session
-	ss := session.SessionStoreInstance()
+func (idc *InitDeployController) getOrgName(orgId string) {
+	org, err := organization.GetOrganizationById(orgId)
 
-	ok, err := ss.ValidateOrgId(sessionId, orgId)
 	if err != nil {
-		mylog.Log.Errorf("Validate Session error: sessionId=%s, error=%s", sessionId, err)
-		idc.Ye = myerror.NewYceError(myerror.EYCE_SESSION, "")
+		log.Errorf("Get Organization By orgId error: orgId=%s, error=%s", orgId, err)
+		idc.Ye = myerror.NewYceError(myerror.EMYSQL_QUERY, "")
 		return
 	}
 
-	// Session invalide
-	if !ok {
-		// relogin
-		mylog.Log.Errorf("Validate Session failed: sessionId=%s, error=%s", sessionId, err)
-		idc.Ye = myerror.NewYceError(myerror.EYCE_SESSION, "")
+	idc.org = org
+	idc.Init.OrgId = orgId
+	idc.Init.OrgName = idc.org.Name
+	log.Debugf("InitDeployController Params: org=%p, orgId=%s", idc.org, idc.Init.OrgId, idc.Init.OrgName)
+
+}
+
+func (idc *InitDeployController) getDatacenters() {
+	// Get Datacenters by a organization
+	idc.Init.DataCenters, err = organization.GetDataCentersByOrganization(idc.org)
+	if err != nil {
+		log.Errorf("Get Organization By orgId error: sessionId=%s, orgId=%s, error=%s", sessionIdFromClient, orgId, err)
+		idc.Ye = myerror.NewYceError(myerror.EMYSQL_QUERY, "")
 		return
 	}
+	return
+}
 
-	mylog.Log.Infof("InitDeployController ValidateSession success")
-
+func (idc *InitDeployController) getAllQuotas() {
+	// Get all quotas
+	idc.Init.Quotas, err = myqouta.QueryAllQuotas()
+	if err != nil {
+		log.Errorf("Get Organization By orgId error: sessionId=%s, orgId=%s, error=%s", sessionIdFromClient, orgId, err)
+		idc.Ye = myerror.NewYceError(myerror.EMYSQL_QUERY, "")
+		return
+	}
 	return
 }
 
@@ -64,110 +69,33 @@ func (idc InitDeployController) Get() {
 	sessionIdFromClient := idc.RequestHeader("Authorization")
 	orgId := idc.Param("orgId")
 
-	mylog.Log.Debugf("InitDeployController Params: sessionId=%s, orgId=%s", sessionIdFromClient, orgId)
-
-	idc.validateSession(sessionIdFromClient, orgId)
-	// Validate OrgId error
-	if idc.Ye != nil {
-		idc.WriteBack()
-		return
-	}
+	log.Debugf("InitDeployController Params: sessionId=%s, orgId=%s", sessionIdFromClient, orgId)
 
 	// Valid session
-	org, err := organization.GetOrganizationById(orgId)
-	if err != nil {
-		mylog.Log.Errorf("Get Organization By orgId error: sessionId=%s, orgId=%s, error=%s", sessionIdFromClient, orgId, err)
-		idc.Ye = myerror.NewYceError(myerror.EMYSQL_QUERY, "")
-		idc.WriteBack()
+	idc.ValidateSession(sessionIdFromClient, orgId)
+	// Validate OrgId error
+	if idc.CheckError() {
 		return
 	}
 
-	idc.org = org
-	idc.Init.OrgId = orgId
-	idc.Init.OrgName = idc.org.Name
-	mylog.Log.Debugf("InitDeployController Params: org=%p, orgId=%s", idc.org, idc.Init.OrgId, idc.Init.OrgName)
-
-	// Get Datacenters by a organization
-	idc.Init.DataCenters, err = organization.GetDataCentersByOrganization(idc.org)
-	if err != nil {
-		mylog.Log.Errorf("Get Organization By orgId error: sessionId=%s, orgId=%s, error=%s", sessionIdFromClient, orgId, err)
-		idc.Ye = myerror.NewYceError(myerror.EMYSQL_QUERY, "")
-		idc.WriteBack()
+	idc.getOrgName(orgId)
+	if idc.CheckError() {
 		return
 	}
 
-	// Get all quotas
-	idc.Init.Quotas, err = myqouta.QueryAllQuotas()
-	if err != nil {
-		mylog.Log.Errorf("Get Organization By orgId error: sessionId=%s, orgId=%s, error=%s", sessionIdFromClient, orgId, err)
-		idc.Ye = myerror.NewYceError(myerror.EMYSQL_QUERY, "")
-		idc.WriteBack()
+	idc.getDatacenters()
+	if idc.CheckError() {
 		return
 	}
 
-	idc.Ye = myerror.NewYceError(myerror.EOK, idc.String())
-	idc.WriteBack()
-	mylog.Log.Infoln("InitDeployController Get over!")
+	idc.getAllQuotas()
+	if idc.CheckError() {
+		return
+	}
+
+	idc.WriteOk(idc.String())
+	log.Infoln("InitDeployController Get over!")
+
 	return
 
 }
-
-/*
-response example:
-{
-	"code": 0,
-	"message": "....",
-	"data": {
-		"orgId":  "1",
-		"orgName": "Ops",
-		"dataCenters": [
-			{
-				"id": "1",
-				"name": "世纪互联",
-				"budget": 10000000,
-				"balance": 10000000
-			},
-			{
-				"id": "2",
-				"name": "电信机房",
-				"budget": 10000000,
-				"balance": 10000000
-			},
-			{
-				"id": "3",
-				"name": "电子城机房",
-				"budget": 10000000,
-				"balance": 10000000
-			}
-		],
-		"quotas": [
-			{
-				"id": "1",
-				"name": "2C4G50G",
-				"cpu": 2,
-				"mem": 4,
-				"rbd": 50,
-				"price": 1000
-			},
-			{
-				"id": "2",
-				"name": "4C8G100G",
-				"cpu": 4,
-				"mem": 8,
-				"rbd": 100,
-				"price": 18000
-			},
-			{
-				"id": "3",
-				"name": "4C16G200G",
-				"cpu": 4,
-				"mem" 16,
-				"rbd": 200,
-				"price": 2860
-
-			}
-
-		]
-	}
-}
-*/
