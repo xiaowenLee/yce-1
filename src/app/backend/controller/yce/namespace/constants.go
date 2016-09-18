c36939b233196c4300a85227044a47656301f99b
lth2015 <lth9379@gmail.com>
2016-09-18 17:33:24 +0800

    add namespace/types.go namespace/constants.go

---
+++ src/app/backend/controller/yce/namespace/constants.go
@@ -0,0 +1,6 @@
+package namespace
+
+const (
+	CPU_MULTIPLIER int64 = 1024
+	MEM_MULTIPLIER int64 = 1024*1024*1024
+)
---
+++ src/app/backend/controller/yce/namespace/types.go
@@ -0,0 +1,7 @@
+package namespace
+
+import (
+	mylog "app/backend/common/util/log"
+)
+
+var log = mylog.Log
