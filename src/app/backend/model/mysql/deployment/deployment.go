07e1a9189d369f5901c1c0f78489664b7fd86ae6
lth2015 <lth9379@gmail.com>
2016-10-10 14:37:04 +0800

    修改dashboard操作记录

--- src/app/backend/model/mysql/deployment/deployment.go
+++ src/app/backend/model/mysql/deployment/deployment.go
@@ -23,9 +23,75 @@ const (
 	DEPLOYMENT_ACTIONTYPE_STAT = "SELECT id, name, actionType, actionVerb, actionUrl, " +
 		"actionAt, actionOp, dcList, success, reason, json, comment, orgId " +
 		"FROM deployment where actionType=?"
+
 	VALID   = 1
 	INVALID = 0
 )
+
+var OPERATION_LOG = `
+SELECT date_format(d.actionAt, '%Y-%m-%d') AS date, sum(d.actionType) AS total, 2 AS op
+FROM deployment d
+WHERE date_format(d.actionAt, '%Y-%m-%d') IN
+  (
+    SELECT DISTINCT date_format(d.actionAt, '%Y-%m-%d')
+    FROM deployment d
+    WHERE date_sub(curdate(), INTERVAL 7 DAY) <= date_format(d.actionAt, '%Y-%m-%d')
+  )
+  AND d.actionType = 2
+GROUP BY date_format(d.actionAt, '%Y-%m-%d')
+
+UNION
+
+SELECT date_format(d.actionAt, '%Y-%m-%d') AS date, sum(d.actionType) AS total, 3 AS op
+FROM deployment d
+WHERE date_format(d.actionAt, '%Y-%m-%d') IN
+  (
+    SELECT DISTINCT date_format(d.actionAt, '%Y-%m-%d')
+    FROM deployment d
+    WHERE date_sub(curdate(), INTERVAL 7 DAY) <= date_format(d.actionAt, '%Y-%m-%d')
+  )
+  AND d.actionType = 3
+GROUP BY date_format(d.actionAt, '%Y-%m-%d')
+
+UNION
+
+SELECT date_format(d.actionAt, '%Y-%m-%d') AS date, sum(d.actionType) AS total, 4 AS op
+FROM deployment d
+WHERE date_format(d.actionAt, '%Y-%m-%d') IN
+  (
+    SELECT DISTINCT date_format(d.actionAt, '%Y-%m-%d')
+    FROM deployment d
+    WHERE date_sub(curdate(), INTERVAL 7 DAY) <= date_format(d.actionAt, '%Y-%m-%d')
+  )
+  AND d.actionType = 4
+GROUP BY date_format(d.actionAt, '%Y-%m-%d')
+
+UNION
+
+SELECT date_format(d.actionAt, '%Y-%m-%d') AS date, sum(d.actionType) AS total, 8 AS op
+FROM deployment d
+WHERE date_format(d.actionAt, '%Y-%m-%d') IN
+  (
+    SELECT DISTINCT date_format(d.actionAt, '%Y-%m-%d')
+    FROM deployment d
+    WHERE date_sub(curdate(), INTERVAL 7 DAY) <= date_format(d.actionAt, '%Y-%m-%d')
+  )
+  AND d.actionType = 8
+GROUP BY date_format(d.actionAt, '%Y-%m-%d')
+
+UNION
+
+SELECT date_format(d.actionAt, '%Y-%m-%d') AS date, sum(d.actionType) AS total, 9 AS op
+FROM deployment d
+WHERE date_format(d.actionAt, '%Y-%m-%d') IN
+  (
+    SELECT DISTINCT date_format(d.actionAt, '%Y-%m-%d')
+    FROM deployment d
+    WHERE date_sub(curdate(), INTERVAL 7 DAY) <= date_format(d.actionAt, '%Y-%m-%d')
+  )
+  AND d.actionType = 9
+GROUP BY date_format(d.actionAt, '%Y-%m-%d')
+`
 
 type Deployment struct {
 	Id         int32  `json:"id"`
@@ -196,4 +262,8 @@ func StatDeploymentByActionType(actionType int) (count int32, err error) {
 	}
 
 	return count, nil
+}
+
+func QueryOperationStat() {
+
 }
