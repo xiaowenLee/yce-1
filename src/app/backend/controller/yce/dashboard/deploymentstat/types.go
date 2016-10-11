1966a4482a4c9a1f38ebdfcd2e478bbdda8bba22
maxwell92 <710028463@qq.com>
2016-10-11 18:50:24 +0800

    change response data structure

--- src/app/backend/controller/yce/dashboard/deploymentstat/types.go
+++ src/app/backend/controller/yce/dashboard/deploymentstat/types.go
@@ -7,17 +7,13 @@ import (
 var log = mylog.Log
 
 type DeploymentsType struct {
+	DcId 	       int32 `json:"dcId"`
+	DcName 	       string `json:"dcName"`
 	DeploymentName string   `json:"deploymentName"`
 	RsName         string   `json:"rsName"`
 	PodName        []string `json:"podName"`
 }
 
-type DatacentersType struct {
-	DcId        int32             `json:"dcId"`
-	DcName      string            `json:"dcName"`
-	Deployments []DeploymentsType `json:"deployments"`
-}
-
 type DeploymentStatType struct {
-	DeploymentStat []DatacentersType `json:"deploymentStat"`
+	DeploymentStat []DeploymentsType `json:"deploymentStat"`
 }
