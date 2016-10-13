61f99908d812a871db34ffc2fe719ee5894f477b
lth2015 <lth9379@gmail.com>
2016-10-13 14:52:44 +0800

    add navListController

--- src/app/backend/controller/yce/navlist/navlist.go
+++ src/app/backend/controller/yce/navlist/navlist.go
@@ -2,14 +2,39 @@ package navlist
 
 import (
 	yce "app/backend/controller/yce"
+	myerror "app/backend/common/yce/error"
+	myuser "app/backend/model/mysql/user"
+	"strconv"
 )
 
 type NavListController struct {
 	yce.Controller
 }
 
-// Get
+// GET /api/v1/organizations/orgId/users/userId/navList
 
 func (nlc NavListController) Get() {
+
+	sessionIdFromClient := nlc.RequestHeader("Authorization")
+	orgId := nlc.Param("orgId")
+	userId := nlc.Param("userId")
+
+	log.Debugf("CreateDeploymentController get Params:  sessionIdFromClient=%s, orgId=%s, userId=%s", sessionIdFromClient, orgId, userId)
+
+	// Validate OrgId error
+	nlc.ValidateSession(sessionIdFromClient, orgId)
+	if cdc.CheckError() {
+		return
+	}
+
+	id, _ := strconv.Atoi(userId)
+
+	user := new(myuser.User)
+	user.QueryUserById(id)
+
+	navList := user.NavList
+
 	nlc.WriteOk(navList)
+	log.Infoln("NavListController over!")
+
 }
