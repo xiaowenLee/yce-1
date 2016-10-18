2ff36d0a1a9b68f12fb973885dc63280204d1a93
maxwell92 <710028463@qq.com>
2016-10-18 21:26:06 +0800

    add check service and endpoints name doc and controller

--- src/app/backend/controller/yce/endpoint/check.go
+++ src/app/backend/controller/yce/endpoint/check.go
@@ -1,1 +1,97 @@
 package endpoint
+
+import (
+	myerror "app/backend/common/yce/error"
+	yce "app/backend/controller/yce"
+	yceutils "app/backend/controller/yce/utils"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/api"
+)
+
+type CheckEndpointsController struct {
+	yce.Controller
+	k8sClients []*client.Client
+	apiServers []string
+
+	params *CheckEndpointsParams
+	orgName string
+}
+
+type CheckEndpointsParams struct {
+	Name string `json:"name"`
+}
+
+func (cec *CheckEndpointsController) check(c *client.Client, name string) {
+	epList, err := c.Endpoints(cec.orgName).List(api.ListOptions{})
+	if err != nil {
+		cec.Ye = myerror.NewYceError(myerror.EKUBE_LIST_ENDPOINTS, "")
+		return
+	}
+
+	for _, ep := range epList.Items {
+		if name == ep.Name {
+			cec.Ye = myerror.NewYceError(myerror.EYCE_EXISTED_NAME, "")
+			return
+		}
+	}
+}
+
+func (cec *CheckEndpointsController) checkDuplicatedName() {
+	for _, c := range cec.k8sClients {
+		cec.check(c, cec.params.Name)
+		if cec.Ye != nil {
+			return
+		}
+	}
+}
+
+func (cec CheckEndpointsController)Post() {
+	cec.params = new(CheckEndpointsParams)
+
+	sessionIdFromClient := cec.RequestHeader("Authorization")
+	orgId := cec.Param("orgId")
+
+	cec.ValidateSession(sessionIdFromClient, orgId)
+	if cec.CheckError() {
+		return
+	}
+
+	err := cec.ReadJSON(cec.params)
+	if err != nil {
+		cec.Ye = myerror.NewYceError(myerror.EJSON, "")
+	}
+	if cec.CheckError() {
+		return
+	}
+
+	cec.orgName, cec.Ye = yceutils.GetOrgNameByOrgId(orgId)
+	if cec.CheckError() {
+		return
+	}
+
+	dcIdList, ye := yceutils.GetDcIdListByOrgId(orgId)
+	if ye != nil {
+		cec.Ye = ye
+	}
+	if cec.CheckError() {
+		return
+	}
+
+	cec.apiServers, cec.Ye = yceutils.GetApiServerList(dcIdList)
+	if cec.CheckError() {
+		return
+	}
+
+	cec.k8sClients, cec.Ye = yceutils.CreateK8sClientList(cec.apiServers)
+	if cec.CheckError() {
+		return
+	}
+
+	cec.checkDuplicatedName()
+	if cec.CheckError() {
+		return
+	}
+
+	cec.WriteOk("")
+	log.Infoln("CheckEndpointsController Check Over!")
+}
--- src/app/backend/controller/yce/service/check.go
+++ src/app/backend/controller/yce/service/check.go
@@ -1,1 +1,96 @@
 package service
+
+import (
+	myerror "app/backend/common/yce/error"
+	yce "app/backend/controller/yce"
+	yceutils "app/backend/controller/yce/utils"
+	client "k8s.io/kubernetes/pkg/client/unversioned"
+	"k8s.io/kubernetes/pkg/api"
+)
+
+type CheckServiceController struct {
+	yce.Controller
+	k8sClients []*client.Client
+	apiServers []string
+
+	params *CheckServiceParams
+	orgName string
+}
+
+type CheckServiceParams struct {
+	Name string `json:"name"`
+}
+
+func (csc *CheckServiceController) check(c *client.Client, name string) {
+	svcList, err := c.Services(csc.orgName).List(api.ListOptions{})
+	if err != nil {
+		csc.Ye = myerror.NewYceError(myerror.EKUBE_LIST_SERVICE, "")
+		return
+	}
+
+	for _, svc := range svcList.Items {
+		if name == svc.Name {
+			csc.Ye = myerror.NewYceError(myerror.EYCE_EXISTED_NAME, "")
+			return
+		}
+	}
+}
+
+func (csc *CheckServiceController) checkDuplicatedName() {
+	for _, c := range csc.k8sClients {
+		csc.check(c, csc.params.Name)
+		if csc.Ye != nil {
+			return
+		}
+	}
+}
+
+func (csc CheckServiceController) Post() {
+	csc.params = new(CheckServiceParams)
+	sessionIdFromClient := csc.RequestHeader("Authorization")
+	orgId := csc.Param("orgId")
+
+	csc.ValidateSession(sessionIdFromClient, orgId)
+	if csc.CheckError() {
+		return
+	}
+
+	err := csc.ReadJSON(csc.params)
+	if err != nil {
+		csc.Ye = myerror.NewYceError(myerror.EJSON, "")
+	}
+	if csc.CheckError() {
+		return
+	}
+
+	csc.orgName, csc.Ye = yceutils.GetOrgNameByOrgId(orgId)
+	if csc.CheckError() {
+		return
+	}
+
+	dcIdList, ye := yceutils.GetDcIdListByOrgId(orgId)
+	if ye != nil {
+		csc.Ye = ye
+	}
+	if csc.CheckError() {
+		return
+	}
+
+	csc.apiServers, csc.Ye = yceutils.GetApiServerList(dcIdList)
+	if csc.CheckError() {
+		return
+	}
+
+	csc.k8sClients, csc.Ye = yceutils.CreateK8sClientList(csc.apiServers)
+	if csc.CheckError() {
+		return
+	}
+
+	csc.checkDuplicatedName()
+	if csc.CheckError() {
+		return
+	}
+
+	csc.WriteOk("")
+	log.Infoln("CheckServiceController Check Over!")
+}
