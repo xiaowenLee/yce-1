1fa6f16c06d95d69217a5d23ee27dbb4fdcd23d6
maxwell92 <710028463@qq.com>
2016-11-14 15:56:05 +0800

    add QueryUsersByOrgId()

--- src/app/backend/model/mysql/user/constants.go
+++ src/app/backend/model/mysql/user/constants.go
@@ -16,6 +16,9 @@ const (
 	USER_SELECT_ALL = "SELECT id, name, password, orgId, createdAt, modifiedAt, modifiedOp, comment FROM user where status=1"
 	USER_CHECK_DUPLICATE_NAME = "SELECT id, name, password, orgId, createdAt, modifiedAt, modifiedOp FROM user WHERE name=? AND status=1"
 	// USER_NAVLIST = "SELECT navList FROM user WHERE id=?"
+	USER_SELECT_BY_ORGID = "SELECT id, name, password, orgId, createdAt, modifiedAt, modifiedOp, comment " +
+		"FROM user " +
+		"WHERE status=1 AND orgId=?"
 
 	VALID   = 1
 	INVALID = 0
