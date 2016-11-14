292b4ab154d0c7e23474063ae00f550e6e7f8d93
maxwell92 <710028463@qq.com>
2016-11-14 14:06:14 +0800

    add list template test

--- src/app/backend/controller/yce/template/list_test.go
+++ src/app/backend/controller/yce/template/list_test.go
@@ -1,1 +1,33 @@
 package template
+
+import (
+	"testing"
+	testclient "app/backend/common/yce/testclient"
+	"fmt"
+)
+
+
+func Test_List(t *testing.T) {
+	header := make(map[string]string)
+	header["Authorization"] = testclient.SessionId
+
+	req := &testclient.Request{
+		Header: header,
+		Path: testclient.LocalServer + "/api/v1/organizations/" + testclient.OrgId + "/users/" + testclient.UserId +
+			"/templates",
+	}
+
+	resp := new(testclient.Response)
+
+	testClient := &testclient.TestClient{
+		Request: *req,
+		Response: *resp,
+	}
+
+	testClient.Get()
+
+	respString := string([]byte(testClient.Response.Body))
+
+	fmt.Println(respString)
+
+}
