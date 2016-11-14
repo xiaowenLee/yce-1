fe8d2fd2f4f184be450fe996002fa4f791431117
maxwell92 <710028463@qq.com>
2016-11-14 14:07:04 +0800

    fix SQL bugs

--- src/app/backend/model/mysql/template/constants.go
+++ src/app/backend/model/mysql/template/constants.go
@@ -5,21 +5,28 @@ const (
 	INVALID = 0
 
 	TEMPLATE_INSERT = "INSERT INTO " +
-		"user(name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment) " +
-		"VALUES(?, ?, ?, ?, ?, ?)"
+		"template(name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment) " +
+		"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+	TEMPLATE_INSERT_ON_DUPLICATE_UPDATE = "INSERT INTO " +
+		"template(name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment) " +
+		"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?) " +
+		"ON DUPLICATE KEY UPDATE status=? "
 	TEMPLATE_UPDATE = "UPDATE template " +
 		"SET name=?, deployment=?, service=?, endpoints=? " +
 		"WHERE id=?"
 	TEMPLATE_DELETE = "UPDATE template " +
-		"SET status=? " +
+		"SET status=?, modifiedAt=?, modifiedOp=? " +
 		"WHERE id=?"
 
-	QUERY_BY_ID = "SELECT id, name, orgId, deployment, service, endpoint, status, createdAt, modifiedAt, modifiedOp, comment) " +
+	QUERY_BY_ID = "SELECT id, name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment " +
 		"FROM template " +
-		"WHERE id=? AND status=1"
+		"WHERE id=? AND status=?"
+	QUERY_BY_NAME = "SELECT id, name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment " +
+		"FROM template " +
+		"WHERE name=? AND status=?"
 	QUERY_ALL_BY_ORGID = "SELECT id, name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment " +
 		"FROM template " +
-		"WHERE orgId=? AND status=1"
+		"WHERE orgId=? AND status=?"
 	QUERY_DUPLICATED_NAME = "SELECT id, name, orgId, deployment, service, endpoints, status, createdAt, modifiedAt, modifiedOp, comment " +
 		"FROM template " +
 		"WHERE name=? AND orgId=?"
