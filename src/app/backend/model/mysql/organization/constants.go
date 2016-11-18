00a9a8116b85e510eee9e9da3a5843b270c8f103
maxwell92 <710028463@qq.com>
2016-11-18 11:51:08 +0800

    adjust organization code location

--- src/app/backend/model/mysql/organization/constants.go
+++ src/app/backend/model/mysql/organization/constants.go
@@ -1,1 +1,28 @@
 package organization
+
+
+const (
+	ORG_SELECT = "SELECT id, name, cpuQuota, memQuota, budget, balance, status, dcIdList," +
+		"createdAt, modifiedAt, modifiedOp, comment " +
+		"FROM organization WHERE id=?"
+	ORG_SELECT_ALL = "SELECT id, name, cpuQuota, memQuota, budget, balance, status, dcIdList," +
+		"createdAt, modifiedAt, modifiedOp, comment " +
+		"FROM organization where status=1"
+
+	ORG_SELECT_NAME = "SELECT id, name, cpuQuota, memQuota, budget, balance, status, dcIdList," +
+		"createdAt, modifiedAt, modifiedOp, comment " +
+		"FROM organization WHERE name=? and status=1"
+
+	ORG_INSERT = "INSERT INTO organization(name, cpuQuota, memQuota, budget, " +
+		"balance, status, dcIdList, createdAt, modifiedAt, modifiedOp, comment) " +
+		"VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+	ORG_UPDATE = "UPDATE organization SET name=?, cpuQuota=?, memQuota=?, budget=?, " +
+		"balance=?, status=?, dcIdList=?, modifiedAt=?, modifiedOp=?, comment=? " +
+		"WHERE id=?"
+
+	ORG_DELETE = "UPDATE organization SET status=?, modifiedAt=?, modifiedOp=? WHERE id=?"
+
+	VALID   = 1
+	INVALID = 0
+)
--- src/app/backend/model/mysql/organization/types.go
+++ src/app/backend/model/mysql/organization/types.go
@@ -1,7 +1,27 @@
 package organization
 
+import (
+	mylog "app/backend/common/util/log"
+)
+
+var log = mylog.Log
+
 type QuotaType struct {
 	CpuQuota int32 `json:"cpuQuota"`
 	MemQuota int32 `json:"memQuota"`
 }
 
+type Organization struct {
+	Id         int32  `json:"id"`
+	Name       string `json:"name"`
+	CpuQuota   int32  `json:"cpu_quota"`
+	MemQuota   int32  `json:"mem_quota"`
+	Budget     string `json:"buget"`
+	Balance    string `json:"balance"`
+	Status     int32  `json:"status"`
+	DcIdList   string `json:"dcIdList"`
+	CreatedAt  string `json:"createdAt"`
+	ModifiedAt string `json:"modifiedAt"`
+	ModifiedOp int32  `json:"modifiedOp"`
+	Comment    string `json:"comment,omitempty"`
+}
