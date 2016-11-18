2888a91032b0bb6fd3680b9bdd3df12c6ed1dc48
maxwell92 <710028463@qq.com>
2016-11-18 11:48:41 +0800

    adjust nodeport code location

--- src/app/backend/model/mysql/nodeport/nodeport.go
+++ src/app/backend/model/mysql/nodeport/nodeport.go
@@ -1,7 +1,6 @@
 package nodeport
 
 import (
-	mylog "app/backend/common/util/log"
 	mysql "app/backend/common/util/mysql"
 	localtime "app/backend/common/util/time"
 	mydatacenter "app/backend/model/mysql/datacenter"
@@ -10,48 +9,7 @@ import (
 	"errors"
 )
 
-var log = mylog.Log
 
-//选择时需要同时满足dcId和port
-const (
-	NP_SELECT = "SELECT svcName, status, createdAt, modifiedAt, modifiedOp, comment " + "FROM nodeport WHERE port=? AND dcId=?"
-
-	NP_SELECT_BY_DC_AND_PORT = "SELECT port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment " + "FROM nodeport WHERE port=? AND dcId=?"
-
-	NP_INSERT = "INSERT INTO " + "nodeport(port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment) " + "VALUES(?, ?, ?, ?, ?, ?, ?, ?)"
-
-	NP_UPDATE = "UPDATE nodeport " + "SET port=?, dcId=?, svcName=?, status=?, modifiedAt=?, modifiedOp=?, comment=? " + "WHERE port=? AND dcId=?"
-
-	NP_DELETE = "UPDATE nodeport " + "SET status=?, modifiedAt=?, modifiedOp=?, comment=? " + "WHERE port=? AND dcId=?"
-
-	NP_INSERT_ON_DUPLICATE_KEY_UPDATE = "INSERT INTO nodeport(port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment) " + "VALUES (?, ?, ?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE svcName=?, status=?, modifiedAt=?, modifiedOp=? "
-
-	NP_SELECT_VALID = "SELECT port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment " + "FROM nodeport WHERE status=? AND dcId=?"
-
-	NP_SELECT_NEW = "SELECT port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment " + "FROM nodeport WHERE port=? AND dcId=? ORDER BY port DESC"
-
-	NP_SELECT_INVALID = "SELECT port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment " + "FROM nodeport WHERE status=0"
-
-	NP_SELECT_VALID_BY_DC_AND_PORT = "SELECT port=?, dcId=?, svcName=?, status=?, createdAt=?, modifiedAt=?, modifiedOp=?, comment=?" + " FROM nodeport WHERE status=1 AND port=? AND dcId=?"
-
-	NP_SELECT_VALID_BY_DC = "SELECT port, dcId, svcName, status, createdAt, modifiedAt, modifiedOp, comment" + " FROM nodeport WHERE status=1 AND dcId=?"
-
-	VALID      = 1
-	INVALID    = 0
-	PORT_START = 30000
-	PORT_LIMIT = 32767
-)
-
-type NodePort struct {
-	Port       int32  `json:"port"`
-	DcId       int32  `json:"dcId"`
-	SvcName    string `json:"svcName"`
-	Status     int32  `json:"status"`
-	CreatedAt  string `json:"createdAt"`
-	ModifiedAt string `jsno:"modifiedAt"`
-	ModifiedOp int32  `json:"modifiedOp"`
-	Comment    string `json:"comment"`
-}
 
 func NewNodePort(port, dcId int32, svcName string, modifiedOp int32) *NodePort {
 	return &NodePort{
